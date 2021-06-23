faa17ee8feb427fd397ebb86d5d7294ece141320
perzikanz <65535030+perzikanz@users.noreply.github.com>
2021-06-23 15:25:07 +0900

    MySQLとの接続確認

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,23 @@
 import express from 'express';
+import mysql from 'mysql2';
+
+import * as dotenv from 'dotenv';
+dotenv.config({ path: `${__dirname}/../.env` });
 
 const app = express();
 const port = 3000;
+
+const con = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DATABASE,
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log('Connected');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
