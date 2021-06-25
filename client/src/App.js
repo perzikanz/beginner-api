a875fb72a666301ff34c08d6b7aa13fed1eed815
perzikanz <65535030+perzikanz@users.noreply.github.com>
2021-06-25 18:15:47 +0900

    idで指定したtodoをDELETEする

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PostTodo from './PostTodo';
 import GetTodo from './GetTodo';
+import DeleteTodo from './DeleteTodo';
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -9,6 +10,7 @@ function App() {
       <h1>ToDo list</h1>
       <PostTodo />
       <GetTodo todos={todos} setTodos={setTodos} />
+      <DeleteTodo />
     </>
   );
 }
