a37ba5c6acc701764e9c14e12c9f08d7555821e0
perzikanz <65535030+perzikanz@users.noreply.github.com>
2021-06-28 16:01:38 +0900

    todoの状態をPUTし、データベースを更新する処理を追加

---
+++ client/src/PutTodo.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+const PutTodo = () => {
+  const [todoId, setTodoId] = useState('');
+  const [checked, setChecked] = useState('true');
+  const idHandleChange = (e) => {
+    setTodoId(e.target.value);
+  };
+  const checkedHandleChange = (e) => {
+    setChecked(e.target.value);
+  };
+  const requestOptions = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ id: todoId, checked: checked }),
+  };
+  const handleSubmit = async () => {
+    console.log(todoId);
+    try {
+      const response = await fetch(
+        'http://localhost:3001/todo',
+        requestOptions
+      );
+      console.log(response.status);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        todoID:
+        <input type='text' value={todoId} onChange={idHandleChange} />
+      </label>
+      <select value={checked} onChange={checkedHandleChange}>
+        <option value='true'>true</option>
+        <option value='false'>false</option>
+      </select>
+      <input type='submit' value='Submit' />
+    </form>
+  );
+};
+
+export default PutTodo;
