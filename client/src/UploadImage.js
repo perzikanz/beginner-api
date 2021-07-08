9249a3dc66f1819c063d49c4bdcf8612daf91665
perzikanz <65535030+perzikanz@users.noreply.github.com>
2021-07-08 17:40:10 +0900

    クリップ機能追加

--- client/src/UploadImage.js
+++ client/src/UploadImage.js
@@ -1,36 +1,68 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useState } from 'react';
+import CropImage from './CropImage';
 
 const UploadImage = () => {
-  const refForm = useRef(null);
+  const previewCanvasRef = useRef(null);
+  const [imageSrc, setImageSrc] = useState(null);
+  const [completedCrop, setCompletedCrop] = useState(null);
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const formData = new FormData(refForm.current);
-    const requestOptions = {
-      method: 'POST',
-      body: formData,
-    };
-    try {
-      await fetch('http://localhost:3001/image', requestOptions);
-    } catch (err) {
-      console.error(err);
+    const canvas = previewCanvasRef.current;
+    const crop = completedCrop;
+
+    if (!crop || !canvas) {
+      return;
     }
+
+    const formData = new FormData();
+    canvas.toBlob(
+      async (blob) => {
+        formData.append('image', blob, 'image.png');
+        const requestOptions = {
+          method: 'POST',
+          body: formData,
+        };
+        try {
+          await fetch('http://localhost:3001/image', requestOptions);
+        } catch (err) {
+          console.error(err);
+        }
+      },
+      'image/png',
+      1
+    );
   };
 
-  useEffect(() => {
-    refForm.current.focus();
-  }, []);
+  const onSelectFile = (event) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const reader = new FileReader();
+      reader.addEventListener('load', () => setImageSrc(reader.result));
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  };
 
   return (
     <>
       <h1>File upload</h1>
-      <form onSubmit={handleSubmit} ref={refForm}>
+      <form onSubmit={handleSubmit}>
         <label>
-          Upload image:
-          <input type='file' name='image' accept='image/*' />
+          Crop image:
+          <input
+            type='file'
+            name='image'
+            accept='image/*'
+            onChange={onSelectFile}
+          />
         </label>
         <input type='submit' value='Submit' />
       </form>
+      <CropImage
+        src={imageSrc}
+        completedCrop={completedCrop}
+        setCompletedCrop={setCompletedCrop}
+        previewCanvasRef={previewCanvasRef}
+      />
     </>
   );
 };
